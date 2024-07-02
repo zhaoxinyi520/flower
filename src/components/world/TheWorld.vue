c58f81d9ba742ef4c15127c0a4b682c2c267396c
winzhaoxinyi <winzhaoxinyi@gmail.com>
2024-07-03 07:19:37 +0800

    .

--- src/components/world/TheWorld.vue
+++ src/components/world/TheWorld.vue
@@ -3,29 +3,15 @@ import { onMounted  } from 'vue'
 import * as THREE from 'three'
 import { Capsule } from 'three/addons/math/Capsule.js';
 import { newRenderer ,drawGround, drawWall, drawSky,addEventFn ,updatePlayer,controls ,updateSpheres,teleportPlayerIfOob} from '@/utils/home/create'
+import {ininScene} from './createScene'
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x88ccee );
-scene.fog = new THREE.Fog( 0x88ccee, 0, 50 );
+ininScene(scene)
+
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.rotation.order = 'YXZ';
-const fillLight1 = new THREE.HemisphereLight( 0x8dc1de, 0x00668d, 1.5 );
-fillLight1.position.set( 2, 1, 1 );
-scene.add( fillLight1 );
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 2.5 );
-directionalLight.position.set( - 5, 25, - 1 );
-directionalLight.castShadow = true;
-directionalLight.shadow.camera.near = 0.01;
-directionalLight.shadow.camera.far = 500;
-directionalLight.shadow.camera.right = 30;
-directionalLight.shadow.camera.left = - 30;
-directionalLight.shadow.camera.top	= 30;
-directionalLight.shadow.camera.bottom = - 30;
-directionalLight.shadow.mapSize.width = 1024;
-directionalLight.shadow.mapSize.height = 1024;
-directionalLight.shadow.radius = 4;
-directionalLight.shadow.bias = - 0.00006;
-scene.add( directionalLight );
+
+
 //创建一个三维场景 asd232  sds
 let container:any;
 const NUM_SPHERES = 100;
